5509df5feab2c0c957a211420f1e7169b77152f1
anowarzz <anowarhosen444@gmail.com>
2022-11-27 12:18:12 +0600

    firebase config file modified

--- src/Shared/Navbar/Navbar.js
+++ src/Shared/Navbar/Navbar.js
@@ -122,7 +122,7 @@ const Navbar = () => {
           className="btn btn-ghost hover:bg-white  normal-case text-2xl md:text-3xl"
         >
           <img src={logo} className="w-16" alt="" />
-          <p className="mx-1 md:mx-3">Resale Mart</p>
+          <p className="mx-1 md:mx-3">Resale Zone</p>
         </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
