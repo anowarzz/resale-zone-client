6f86c50dc49277d5b6b1283a7bbecaec33d7a580
anowarzz <anowarhosen444@gmail.com>
2022-11-27 13:08:06 +0600

    loaded categories data using axios

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeroSection from '../HeroSection/HeroSection';
+import ProductCategoryList from '../ProductCategoryList/ProductCategoryList';
 
 const Home = () => {
     return (
         <div>
             <HeroSection />
+            <ProductCategoryList />
         </div>
     );
 };
