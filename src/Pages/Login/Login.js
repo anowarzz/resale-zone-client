4f454824651a1c7dd998f1792fa07ee15c6d7b11
anowarzz <anowarhosen444@gmail.com>
2022-11-28 15:42:52 +0600

    login page problems solved

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,21 +1,19 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState } from 'react';
 import { GoogleAuthProvider } from "firebase/auth";
-import { useForm } from "react-hook-form";
-import { AuthContext } from "../../contexts/AuthProvider";
-import { toast } from "react-toastify";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useForm } from 'react-hook-form';
+import { AuthContext } from '../../contexts/AuthProvider';
+import { toast } from 'react-toastify';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 const googleProvider = new GoogleAuthProvider();
 
-const LogIn = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+const Login = () => {
+
+
+
+  const {register,handleSubmit,formState: { errors },} = useForm();
   const { signIn, googleLogIn, updateUser } = useContext(AuthContext);
   const [loginError, setLoginError] = useState("");
-
 
   // location
   const location = useLocation();
@@ -27,6 +25,7 @@ const LogIn = () => {
 
   // Login user using email and password
   const handleLogin = (data, e) => {
+    e.preventDefault();
     console.log(data);
     setLoginError("");
 
@@ -79,29 +78,10 @@ const LogIn = () => {
   const saveUserToDB = (name, email, userType, userImg) => {
     const user = { name, email, userType, userImg };
 
-  try{
     fetch("http://localhost:5000/users", {
       method: "POST",
       headers: {
-        "content-type": "application/json",
-        
-      },
-      body: JSON.stringify(user),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-
-      });
-  }
-  catch (error){
-console.log(error);
-
-
-    fetch("http://localhost:5000/users", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-        
+        "content-type": "application/json",  
       },
       body: JSON.stringify(user),
     })
@@ -109,15 +89,16 @@ console.log(error);
       .then((data) => {
       });
 
-  }
+    }
 
 
   return (
     <div className="flex flex-col justify-center items-center mb-4">
-      <div className="max-w-96 md:w-auto px-16 py-4 border border-gray-200  shadow-slate-500 shadow-lg bg-slate-200">
-        <h2 className="text-2xl md:text-3xl text-center my-6">Login</h2>
+    <div className="max-w-96 md:w-auto px-16 py-4 border border-gray-200  shadow-slate-500 shadow-lg bg-slate-200">
+      <h2 className="text-2xl md:text-3xl text-center my-6">Login</h2>
 
-        <form onSubmit={handleSubmit(handleLogin)}>
+
+      <form onSubmit={handleSubmit(handleLogin)}>
           <div className="form-control w-full max-w-xs">
             <label className="label">
               <span className="label-text ">Email</span>
@@ -173,36 +154,35 @@ console.log(error);
             type="submit"
           />
         </form>
-
-        <p className="mt-4">
-          New to Resale Mart ?
-          <Link
-            to="/register"
-            className="text-primary font-semibold  pl-1 hover:link"
-          >
-            Register Now
-          </Link>
-        </p>
-        <div className="divider">OR</div>
-
-        <button
-          onClick={handleGoogleLogin}
-          aria-label="Login with Google"
-          type="button"
-          className="flex items-center justify-center w-full p-2 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 border-gray-600 focus:ring-violet-600 hover:bg-accent hover:text-white"
+      <p className="mt-4">
+        New to Resale Mart ?
+        <Link
+          to="/register"
+          className="text-primary font-semibold  pl-1 hover:link"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 32 32"
-            className="w-5 h-5 fill-current"
-          >
-            <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
-          </svg>
-          <p>Continue with Google</p>
-        </button>
-      </div>
-    </div>
-  );
-};}
+          Register Now
+        </Link>
+      </p>
+      <div className="divider">OR</div>
 
-export default LogIn;
+      <button
+        onClick={handleGoogleLogin}
+        aria-label="Login with Google"
+        type="button"
+        className="flex items-center justify-center w-full p-2 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 border-gray-600 focus:ring-violet-600 hover:bg-accent hover:text-white"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 32 32"
+          className="w-5 h-5 fill-current"
+        >
+          <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
+        </svg>
+        <p>Continue with Google</p>
+      </button>
+    </div>
+  </div>
+  );
+};
+
+export default Login;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -2,11 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 import DashboardLayout from "../layout/DashboardLayout";
 import Main from "../layout/Main";
 import Blog from "../Pages/Blog/Blog";
-import Dashboard from "../Pages/Dashboard/Dashboard/Dashboard";
 import MyOrders from "../Pages/Dashboard/MyOrders/MyOrders";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
+
 import Products from "../Pages/Products/Products";
 import Register from "../Pages/Register/Register";
 import PrivateRoute from "./PrivateRoute/PrivateRoute";
@@ -21,8 +21,9 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home />,
       },
+  
       {
-        path: "/login",
+        path: '/login',
         element: <Login />
       },
       {
@@ -35,7 +36,7 @@ const router = createBrowserRouter([
       },
       {
         path: '/category/:id',
-        element: <PrivateRoute> <Products /> </PrivateRoute>,
+        element: <PrivateRoute>  <Products />  </PrivateRoute>,
         loader: async ({params}) => {
           const res = await fetch(`http://localhost:5000/categoryProducts?id=${params.id}`, {
             headers: {
